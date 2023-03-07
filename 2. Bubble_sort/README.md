239b19692fdb1fa3e679823907cc0de3990a0b84
Kunal Kumar <kunal.kumar@etelligens.in>
2023-03-07 17:37:09 +0530

    add readme file

---
+++ 2. Bubble_sort/README.md
@@ -0,0 +1,10 @@
+# BUBBLE SORT
+
+![BUBBLE SORT](../assets/bubble_sort.png)
+
+> BubbleSort Pseudocode
+
+1. Start looping from with a variable called i the end of the array towards the beginning
+2. Start an inner loop with a variable called j from the beginning until i - 1
+3. If arr[j] is greater than arr[j+1], swap those two values!
+4. Return the sorted array
--- 2. Bubble_sort/Sorting_bubble.js
+++ 2. Bubble_sort/Sorting_bubble.js
@@ -1,28 +1,54 @@
+// es5
 function bubbleSort(arr) {
-
-    function swap(arr,left,right) {
-        [arr[left],arr[right]] = [arr[right],arr[left]]
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      // Swap the position on some condition
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
-
-    for (let i = arr.length; i > 0; i--) {
-        for (let j = 0; j < i - 1; j++) {
-            // Swap the position on some condition
-            
-            // es5
-            // if (arr[j] > arr[j + 1]) {
-            //     var temp = arr[j];
-            //     arr[j] = arr[j + 1];
-            //     arr[j + 1] = temp
-            // }
-
-            // es6
-            if(arr[j] > arr[j+1]){
-                swap(arr,j,j+1)
-            }
-        }
-    }
-    console.log(arr)
-    return arr
+  }
+  //   console.log(arr);
+  return arr;
 }
 
-bubbleSort([2, 4, 3, 9, 5])
+// es6
+function bubbleSort(arr) {
+  function swap(arr, left, right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+  }
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      // Swap the position on some condition
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+// Optamized with noSwap
+function bubbleSort(arr) {
+  var noSwap;
+  for (let i = arr.length; i > 0; i--) {
+    noSwap = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {         //   Swap the position on some condition
+          var temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp
+          noSwap = false;
+      }
+    }
+    if(noSwap) break;
+  }
+  console.log(arr);
+  return arr;
+}
+
+bubbleSort([2, 4, 3, 9, 5]);
