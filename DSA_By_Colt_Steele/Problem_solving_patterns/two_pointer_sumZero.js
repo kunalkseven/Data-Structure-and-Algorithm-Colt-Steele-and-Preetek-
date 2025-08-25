8e069fbed735cd882277bfff66db0a9183298d50
Kunal Kumar <64822728+kunalkseven@users.noreply.github.com>
2025-08-25 17:10:59 +0000

    two_pointer_sumZero

---
+++ DSA_By_Colt_Steele/Problem_solving_patterns/two_pointer_sumZero.js
@@ -0,0 +1,20 @@
+// Question sumZero([-3,-2,-1,0,1,2,3])  ===> [-3,3]
+
+
+function sumZero(arr){
+    let left = 0
+    let right = arr.length - 1
+
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [left,right];
+        }else if(sum > 0){
+            right--;
+        }else{
+            left++;
+        }
+    }
+}
+
+console.log(sumZero([-3,-2,-1,0,1,2,3]))
